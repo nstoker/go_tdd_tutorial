b5b7649e57de82da1ba52ff733935a9528ecfafd
Neil Stoker <neil.stoker@gmail.com>
2021-04-08 21:08:12 +0100

    Refactor app into folder

--- main.go
+++ main.go
@@ -4,12 +4,13 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/nstoker/go_tdd_tutorial/app"
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	godotenv.Load(".env")
-	a := App{}
+	a := app.App{}
 	a.Initialize(os.Getenv("DATABASE_URL"))
 	port := os.Getenv("PORT")
 	if port == "" {
